d09ab0c8037c2f7823d9c858248f22d2c879fc6c
aasane kariuki <franciskariki053@gmail.com>
2024-05-21 18:57:02 +0300

    lab done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+ 
+ print('Hello, programmer!')
+ 
+ pass
 
-def greet(name):
+def greet(name = 'aasane' ):
+
+    print (f"Hello, {name}!")
+
+    
     pass
 
 def greet_with_default(name="programmer"):
+
+
+    print(f"Hello, {name}!")
+
+
     pass
+
 
 def add(num1, num2):
+
+    return num1 + num2
+
+    print(add(45, 55))
     pass
+   
 
 def halve(number):
+
+    return  number / 2
+
     pass
